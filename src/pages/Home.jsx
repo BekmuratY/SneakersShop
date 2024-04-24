ce566ca998bfd0aed77cb1753b522a07d205b916
BekmuratY <esebb530@gmail.com>
2024-04-24 20:21:42 +0200

    Добавлены кнопки с сортировкой в компонент Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../components/Card';
-
 
 function Home({
     items,
-    cartItems ,
+    cartItems,
     searchValue,
     setSearchValue,
     onChangeSearchInput,
@@ -12,38 +11,57 @@ function Home({
     onAddToCart,
     isLoading,
 }) {
+    const [sortBy, setSortBy] = useState('price');
+    const [sortOrder, setSortOrder] = useState('asc');
+
+    const handleSortChange = (type) => {
+        if (type === sortBy) {
+            setSortOrder(order => (order === 'asc' ? 'desc' : 'asc'));
+        } else {
+            setSortBy(type);
+            setSortOrder('asc');
+        }
+    };
+
     const renderItems = () => {
         const filteredItems = items.filter((item) => item && item.title && item.title.toLowerCase().includes(searchValue.toLowerCase()));
-    
-        return (isLoading ? [...Array(12)] : filteredItems).map((item, index) => (
+
+        const sortedItems = filteredItems.slice().sort((a, b) => {
+            if (sortBy === 'price') {
+                return sortOrder === 'asc' ? a.price - b.price : b.price - a.price;
+            } else if (sortBy === 'title') {
+                return sortOrder === 'asc' ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+            }
+        });
+
+        return (isLoading ? [...Array(12)] : sortedItems).map((item, index) => (
             <Card 
                 key={index}
-                title={item && item.title} // Добавляем дополнительную проверку
-                price={item && item.price} // Добавляем дополнительную проверку
-                imageUrl={item && item.imageUrl} // Добавляем дополнительную проверку
+                title={item && item.title}
+                price={item && item.price}
+                imageUrl={item && item.imageUrl}
                 onFavorite={(obj) => onAddFavorite(obj)}
                 onPlus={(obj) => onAddToCart(obj)}
-                id={item && item.id} // Добавляем дополнительную проверку
+                id={item && item.id}
                 {...item}
-                
                 loading={isLoading}
             />
         ));
-    }
-    
-    
-    
-    
+    };
+
     return (
         <div className="content p-40">
-            <div className="d-flex align-center justify-between mb-40">
-                <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : "Каталог " }</h1>
+            <div className="d-flex align-center justify-between mb-20">
+                <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : "Каталог"}</h1>
                 <div className="searsh-block d-flex">
                     <img src="/img/searsh.svg" alt="Search"></img>
                     <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."></input>
-                </div>
+                </div> 
             </div>
-
+            <div className= "mb-20">
+                    <button className = "sorting" onClick={() => handleSortChange('price')}>Сортировать по цене</button>
+                    <button className = "sorting" onClick={() => handleSortChange('title')}>Сортировать по алфавиту</button> 
+                </div>
 
             <div className="d-flex flex-wrap">
                 {renderItems()}
